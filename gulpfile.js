08a3df82850c0a61d7912f78633f0c59f8e50a34
damianwajser <wajser@gmail,com>
2018-04-19 17:41:24 -0300

    seo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 gulp.task('image', function(){
-	gulp.src('images/*/*').pipe(imagemin())
+	gulp.src('images/**/*').pipe(imagemin([
+		imagemin.gifsicle({interlaced: true}),
+		imagemin.jpegtran({progressive: true}),
+		imagemin.optipng({optimizationLevel: 5}),
+		imagemin.svgo({
+			plugins: [
+				{removeViewBox: true},
+				{cleanupIDs: false}
+			]
+		})
+	]))
 	.pipe(gulp.dest('images'));
 });
 gulp.task('default', [ 'image' ]);
